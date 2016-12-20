4f6959dc9b1394ca30f6754b66b45fc0bf4f635b
Louis Lainé <darksioul6@gmail.com>
2016-12-20 14:22:39 +0100

    feat: merge multiple projects into one

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,6 +4,54 @@ import LokaliseService from '../services/lokalise';
 
 import type { ProjectParams } from '../services/lokalise';
 
+type QueryString = {
+  project: Array<string>;
+  lang: string;
+  format: string;
+  reload: boolean
+};
+
+/**
+ * Transform a string separated with commas to an array.
+ * @param projects
+ * @returns {Array}
+ */
+function sanitizeProjects(projects: string): Array<string> {
+  const commaSeparator: string = ',';
+  const arr: Array<string> = projects.split(commaSeparator);
+  return arr.map(item => item.trim());
+}
+
+/**
+ * This function is responsible to handle the &reload=true case where the user
+ * ask specifically to reload the assets for one project.
+ * @param project
+ * @returns {Promise.<*>}
+ */
+function fetchOneProject(project: ProjectParams): Promise<*> {
+  if (project.reload) {
+    return LokaliseService.getTranslationFromApi(project);
+  }
+
+  return LokaliseService.getTranslations(project);
+}
+
+/**
+ * Responsible to fetch multiple project and return them as an array of promises.
+ * @param params
+ * @returns {Promise.<*>}
+ */
+function fetchMultipleProjects(params: QueryString): Promise<*> {
+  return Promise.all(params.project.map(projectName => {
+    const project: ProjectParams = {
+      ...params,
+      project: projectName
+    };
+
+    return fetchOneProject(project);
+  }));
+}
+
 /**
  * Index controller for / route
  * @param req
@@ -11,8 +59,8 @@ import type { ProjectParams } from '../services/lokalise';
  * @param next
  */
 export function index(req: any, res: any, next: any) {
-  const params: ProjectParams = {
-    project: req.swagger.params.project.value,
+  const params: QueryString = {
+    project: sanitizeProjects(req.swagger.params.project.value),
     lang: req.swagger.params.lang.value,
     format: req.swagger.params.format.value || 'json',
     reload: req.swagger.params.reload.value
@@ -20,11 +68,12 @@ export function index(req: any, res: any, next: any) {
 
   Promise.resolve()
     .then(() => {
-      if (params.reload) {
-        return LokaliseService.getTranslationFromApi(params);
+      if (params.project.length > 1) {
+        return fetchMultipleProjects(params);
       }
-      return LokaliseService.getTranslations(params);
+
+      return fetchOneProject({ ...params, project: params.project[0] });
     })
-    .then(response => res.send(response))
+    .then(result => res.send(result))
     .catch(next);
 }
